f47b7897c774c7bd6bdce195e6a37d38b89e100f
Michel Gawron <michel.gawron@devinci.fr>
2018-02-23 15:29:33 +0100

    git init

---
+++ app/src/main/java/com/example/michlg/youtubebaseapplication/YoutubeClient.java
@@ -0,0 +1,11 @@
+package com.example.michlg.youtubebaseapplication;
+
+import retrofit2.http.GET;
+
+/**
+ * Created by michlg on 23/02/2018.
+ */
+
+public interface YoutubeClient {
+    @GET("")
+}
